985d99a9bd59e3ded948f8ef2bbab72d1ba714aa
Alexander Necheff <alex@necheff.net>
2023-10-24 00:21:47 -0400

    add copyright and license notice.

--- argon2id_suite_test.go
+++ argon2id_suite_test.go
@@ -1,3 +1,21 @@
+/*
+   Copyright (C) 2023 Alexander Necheff
+
+   This file is part of argon2id.
+
+   argon2id is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, at version 3 of the License.
+
+   argon2id is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with argon2id.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package argon2id_test
 
 import (
